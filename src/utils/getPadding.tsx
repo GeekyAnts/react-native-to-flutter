49f46362e966ac2d599f5f79b3d535390fcdbea6
hasnentai <hasnen712@live.com>
2022-12-02 11:31:33 +0530

    Feat: Added Position support

---
+++ src/utils/getPadding.tsx
@@ -0,0 +1,54 @@
+import { dartType } from "..";
+
+
+
+export const getPadding = (styles: any, object: any) => {
+  object.properties = [];
+  debugger
+  if (styles.hasOwnProperty("padding")) {
+
+    delete object.properties;
+  }
+  else {
+
+    object.class = "EdgeInsets.only";
+    if (styles.hasOwnProperty("paddingLeft") || styles.hasOwnProperty("paddingHorizontal")) {
+
+      let widget: any = dartType.double;
+      widget.value = styles["paddingLeft"] ?? styles["paddingHorizontal"];
+      widget = { ...widget, namedProp: "left" };
+      object.properties.push(widget);
+
+    }
+
+    if (styles.hasOwnProperty("paddingRight")|| styles.hasOwnProperty("paddingHorizontal")) {
+
+      let widget: any = dartType.double;
+      widget.value = styles["paddingRight"]?? styles["paddingHorizontal"];;
+      widget = { ...widget, namedProp: "right" };
+      object.properties.push(widget);
+
+    }
+
+    if (styles.hasOwnProperty("paddingTop") || styles.hasOwnProperty("paddingVertical")) {
+
+      let widget: any = dartType.double;
+      widget.value = styles["paddingTop"] ?? styles["paddingVertical"];;
+      widget = { ...widget, namedProp: "top" };
+      object.properties.push(widget);
+
+    }
+
+    if (styles.hasOwnProperty("paddingBottom") || styles.hasOwnProperty("paddingVertical")) {
+
+      let widget: any = dartType.double;
+      widget.value = styles["paddingBottom"]?? styles["paddingVertical"];
+      widget = { ...widget, namedProp: "bottom" };
+      object.properties.push(widget);
+
+    }
+
+  }
+
+  return object;
+};
